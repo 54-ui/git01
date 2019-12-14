3cddfdb02ace96b40c28ea8b8b49db31ab2bb7fe
54-ui <1658344033@qq.com>
2019-12-14 15:17:28 +0800

    这是我xin提交的代码！

--- src/main/java/com/zking/App.java
+++ src/main/java/com/zking/App.java
@@ -4,9 +4,10 @@ package com.zking;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ) {
+        int a = 10;
+        int b = 5;
         System.out.println( "Hello World!" );
     }
 }
